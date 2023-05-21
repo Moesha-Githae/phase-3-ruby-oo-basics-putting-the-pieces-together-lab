94acb20336a854824a4e1411fe22f2b03f336ec9
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-05-21 12:33:26 +0300

    complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class book
+    attr_accessor :author, :page_count, :genre, :title
 
+    def intialize(title)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts "flipping the page .. wow , you read first"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+require 'pry'
+class shoe 
+    attr_accessor :brand, :color, :materail, :condition
+
+    def intialize(brand)
+        @brand = brand
+        @color =color
+        @materail = materail
+        @condition = condition
+    end
+
+    def coble 
+        puts "your shoe is as good as new"
+    end
+end
